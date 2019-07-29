a21b7ff7928d0c88a756bdc21b7b94b271bd1528
valasruthi <valasruthi4@mail.com>
2019-07-29 14:52:31 +0530

    autowire bytype byname

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -12,14 +12,6 @@ public class Movie {
 @Autowired
   private Actor actor;
 
-  public Movie() {
-  }
-
-  public Movie(Actor actor) {
-    this.actor = actor;
-  }
-
-
   @Override
   public String toString() {
     return "Movie{" +
